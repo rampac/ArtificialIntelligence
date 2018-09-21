27990253417d72d082d83b8b54bead58677ae5c5
Ramesh Pachiannan <rpachiannan@wsgc.com>
2018-09-21 16:41:08 -0700

    Commits on Polinomial Regression

--- SciKit_Learn/LogisticRegression.py
+++ SciKit_Learn/LogisticRegression.py
@@ -2,7 +2,7 @@ import pandas as vAR_pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 
-vAR_df = vAR_pd.read_excel('C:/Ramesh/AI/1_AI Boot Camp Problem1 - Intercompany_Transaction_Train.xlsx')
+vAR_df = vAR_pd.read_excel('C:/Users/rpachiannan/PycharmProjects/AI_GIT/ArtificialIntelligence/Data/1 - Intercompany_Transaction_Train.xlsx')
 
 vAR_le = LabelEncoder()
 vAR_Transaction_Type_Conversion = vAR_le.fit_transform(vAR_df.iloc[:,7])
@@ -41,7 +41,7 @@ model.fit(vAR_Features_train,vAR_Label_train)
 
 model.predict(vAR_Features_train)
 
-vAR_df3 = vAR_pd.read_excel('C:/Ramesh/AI/1_AI Boot Camp Problem1 - Intercompany_Transaction_Test.xlsx')
+vAR_df3 = vAR_pd.read_excel('C:/Users/rpachiannan/PycharmProjects/AI_GIT/ArtificialIntelligence/Data/1 - Intercompany_Transaction_Test.xlsx')
 vAR_Transaction_Type_Conversion_test = vAR_le.fit_transform(vAR_df3.iloc[:,3])
 vAR_Transaction_Type_Conversion_test_df = vAR_pd.DataFrame(vAR_Transaction_Type_Conversion_test,columns={'Transaction_Type_Converted'})
 vAR_Data_Category_Conversion_test = vAR_le.fit_transform(vAR_df.iloc[:,4])
@@ -58,9 +58,9 @@ vAR_Labels_Pred = model.predict(vAR_Features_test)
 vAR_Labels_Pred = vAR_pd.DataFrame(vAR_Labels_Pred,columns={'Predicted_Inter_Transaction_Type'})
 #vAR_Features_test = vAR_Labels_Pred.sort()
 
-vAR_df6 = vAR_pd.read_excel('C:/Ramesh/AI/1_AI Boot Camp Problem1 - Intercompany_Transaction_Test.xlsx')
+vAR_df6 = vAR_pd.read_excel('C:/Users/rpachiannan/PycharmProjects/AI_GIT/ArtificialIntelligence/Data/1 - Intercompany_Transaction_Test.xlsx')
 vAR_df7 = vAR_df6.merge(vAR_Labels_Pred,left_index=True, right_index=True)
-vAR_df8 = vAR_df7.to_excel('C:/Ramesh/AI/Problem1_Predicted_Results.xlsx')
+vAR_df8 = vAR_df7.to_excel('C:/Users/rpachiannan/PycharmProjects/AI_GIT/ArtificialIntelligence/Data/Problem1_Predicted_Results.xlsx')
 
-vAR_df9 = vAR_pd.read_excel('C:/Ramesh/AI/Problem1_Predicted_Results.xlsx')
+vAR_df9 = vAR_pd.read_excel('C:/Users/rpachiannan/PycharmProjects/AI_GIT/ArtificialIntelligence/Data/Problem1_Predicted_Results.xlsx')
 print(vAR_df9)
